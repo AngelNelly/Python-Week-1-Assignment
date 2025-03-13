b0ce671a072eb1c533ec1a261eb0009a988dcc78
AngelNelly <talktoangelnelly@gmail.com>
2025-03-13 16:25:20 +0100

    Asking the user for operation

--- assign1.py
+++ assign1.py
@@ -1,2 +1,5 @@
-num1 = float(input("Enter the first number:"))
-num2 = float(input("Enter the second number")) 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+operation = input("Enter an operation (+,-, *, /): ")
+
